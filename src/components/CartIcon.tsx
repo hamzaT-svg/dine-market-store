d5059e1ee2a1bc24f677f233240e6cbc8578912c
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2023-06-08 18:36:33 +0500

    Issue FIxed

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -30,14 +30,14 @@ const getTotalOrders = async (): Promise<Count> => {
   }
 };
 const CartIcon = () => {
-  // const totalOrders = use(getTotalOrders());
+  const totalOrders = use(getTotalOrders());
 
   return (
     <Link href={"/cart"}>
       <div className="bg-[#F1F1F1] relative w-11 h-11 rounded-full flex items-center justify-center cursor-pointer duration-300 hover:scale-105">
         <Cart style={{ fontSize: "1.3rem" }} />
         <h1 className="bg-[#f02d34] text-[#EEEEEE] rounded-full w-3 h-3 text-center text-[10px] absolute top-1.5 right-1.5">
-          {/* {totalOrders} */}
+          {totalOrders}
         </h1>
       </div>
     </Link>
